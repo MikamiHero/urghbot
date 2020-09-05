aaef9b09794ea6472f60c7c169f8762d9f17bafa
mikamihero <mikamihero@gmail.com>
2020-09-05 11:42:47 +1000

    Forgot to remove the mod 2 logic (should be mod 5)

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on("chat", async (channel, user, message, self) => {
   }
   //  Generate random number and check if it's divisible by 5
   const x = randInt(1, 200);
-  if (x % 2 === 0) {
+  if (x % 5 === 0) {
     // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
     const ughString = await messageToUgh(message);
     if (ughString !== "") {
