a5efbb9d10722ab86ed4aea4611abb68198db5bb
mikamihero <mikamihero@gmail.com>
2020-09-07 14:09:14 +1000

    Changed the frequency that UrghBot will trigger (20% down to 7.5%)

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ client.on("chat", async (channel, user, message, self) => {
   if (message.toLowerCase().includes("glitches")) {
     client.say(twitchChannel, "Ugh. Glitches? More like cheaters.");
   }
-  //  Generate random number and check if it's divisible by 5
+  //  Generate random number and check if it's divisible by 13 (15/200 numbers = 7.5% chance of firing)
   const x = randInt(1, 200);
-  if (x % 5 === 0) {
+  if (x % 13 === 0) {
     // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
     const ughString = await messageToUgh(message);
     if (ughString !== "") {
