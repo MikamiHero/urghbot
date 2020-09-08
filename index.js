7a5e3f52149a9207aafdc150e25740dcaacb31b4
mikamihero <mikamihero@gmail.com>
2020-09-08 12:25:36 +1000

    Adding a try-catch in case something goes horribly wrong

--- index.js
+++ index.js
@@ -47,34 +47,47 @@ client.on("connected", (address, port) => {
 
 // Commands and/or certain character triggers
 client.on("chat", async (channel, user, message, self) => {
-  // Don't want to react to itself
-  if (self) {
-    return;
-  }
-  // If someone wants to praise UrghBot
-  else if (message === "urghbot yes" || message === "@urghbot yes") {
-    client.say(twitchChannel, `PogChamp`);
-  }
-  // If someone wants to scold UrghBot
-  else if (message === "urghbot no" || message === "@urghbot no") {
-    client.say(twitchChannel, "BibleThump");
-  }
-  // If someone wants to 'ugh' or 'urgh' themselves
-  else if (message === "!ugh" || message == "!urgh") {
-    client.say(twitchChannel, `Ugh. ${user["display-name"]}.`);
-  }
-  // If the message contains 'glitches'
-  else if (message.toLowerCase().includes("glitches")) {
-    client.say(twitchChannel, "Ugh. Glitches? More like cheaters.");
-  } else {
-    //  Generate random number and check if it's divisible by 13 (15/200 numbers = 7.5% chance of firing)
-    const x = randInt(1, 200);
-    if (x % 13 === 0) {
-      // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
-      const ughString = await messageToUgh(message);
-      if (ughString !== "") {
-        client.say(twitchChannel, ughString);
+  try {
+    // Don't want to react to itself
+    if (self) {
+      return;
+    }
+    // TODO: add logic to return if the user is meant to be ignored
+    // If someone wants to have themselves ignored by urghbot
+    else if (message === "!ignore") {
+      client.say(twitchChannel, `Ugh. Bye. <ignores ${user["display-name"]}>`);
+    }
+    // If someone wants to be acknowledged by urghbot again
+    else if (message === "!unignore") {
+      client.say(twitchChannel, `Ugh. Fine. <unignores ${user["display-name"]}>`);
+    }
+    // If someone wants to praise UrghBot
+    else if (message === "urghbot yes" || message === "@urghbot yes") {
+      client.say(twitchChannel, `PogChamp`);
+    }
+    // If someone wants to scold UrghBot
+    else if (message === "urghbot no" || message === "@urghbot no") {
+      client.say(twitchChannel, "BibleThump");
+    }
+    // If someone wants to 'ugh' or 'urgh' themselves
+    else if (message === "!ugh" || message == "!urgh") {
+      client.say(twitchChannel, `Ugh. ${user["display-name"]}.`);
+    }
+    // If the message contains 'glitches'
+    else if (message.toLowerCase().includes("glitches")) {
+      client.say(twitchChannel, "Ugh. Glitches? More like cheaters.");
+    } else {
+      //  Generate random number and check if it's divisible by 13 (15/200 numbers = 7.5% chance of firing)
+      const x = randInt(1, 200);
+      if (x % 13 === 0) {
+        // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
+        const ughString = await messageToUgh(message);
+        if (ughString !== "") {
+          client.say(twitchChannel, ughString);
+        }
       }
     }
+  } catch (err) {
+    client.say(twitchChannel, `@${twitchChannel}, I think we have a problem...`);
   }
 });
