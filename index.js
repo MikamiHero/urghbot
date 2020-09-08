90849f7332726fa48446c05dce5235e4b15ee936
mikamihero <mikamihero@gmail.com>
2020-09-08 12:15:17 +1000

    Putting everything into if-else to prevent double firing by accident

--- index.js
+++ index.js
@@ -52,28 +52,29 @@ client.on("chat", async (channel, user, message, self) => {
     return;
   }
   // If someone wants to praise UrghBot
-  if (message === "urghbot yes") {
+  else if (message === "urghbot yes") {
     client.say(twitchChannel, `PogChamp`);
   }
   // If someone wants to scold UrghBot
-  if (message === "urghbot no") {
+  else if (message === "urghbot no") {
     client.say(twitchChannel, "BibleThump");
   }
   // If someone wants to 'ugh' or 'urgh' themselves
-  if (message === "!ugh" || message == "!urgh") {
+  else if (message === "!ugh" || message == "!urgh") {
     client.say(twitchChannel, `Ugh. ${user["display-name"]}.`);
   }
   // If the message contains 'glitches'
-  if (message.toLowerCase().includes("glitches")) {
+  else if (message.toLowerCase().includes("glitches")) {
     client.say(twitchChannel, "Ugh. Glitches? More like cheaters.");
-  }
-  //  Generate random number and check if it's divisible by 13 (15/200 numbers = 7.5% chance of firing)
-  const x = randInt(1, 200);
-  if (x % 13 === 0) {
-    // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
-    const ughString = await messageToUgh(message);
-    if (ughString !== "") {
-      client.say(twitchChannel, ughString);
+  } else {
+    //  Generate random number and check if it's divisible by 13 (15/200 numbers = 7.5% chance of firing)
+    const x = randInt(1, 200);
+    if (x % 13 === 0) {
+      // If they are the lucky one, 'ugh'-ify the user's message (if there is a noun in it)
+      const ughString = await messageToUgh(message);
+      if (ughString !== "") {
+        client.say(twitchChannel, ughString);
+      }
     }
   }
 });
