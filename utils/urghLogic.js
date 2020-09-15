82f25bfe90aa963c54ade2531f1578071e9c46d9
mikamihero <mikamihero@gmail.com>
2020-09-15 18:07:10 +1000

    Reomved console.log

--- utils/urghLogic.js
+++ utils/urghLogic.js
@@ -1,7 +1,6 @@
 const isProperUrghCommand = (message) => {
   // This will render either !ugh or !urgh
   const startOfMessage = message.split(" ")[0];
-  console.log(startOfMessage === "!urgh" || startOfMessage === "!ugh");
   return startOfMessage === "!urgh" || startOfMessage === "!ugh";
 };
 
