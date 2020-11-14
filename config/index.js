f685bc975b18f9136b43e6b62f90d6e367a67c48
mikamihero <mikamihero@gmail.com>
2020-11-14 22:47:36 +1100

    Added a Twitch utils for fetching the user details via username

--- config/index.js
+++ config/index.js
@@ -11,6 +11,10 @@ config.mongodbUrghBotURI = process.env.MONGODB_URGHBOT_URI;
 config.botUsername = process.env.TWITCH_BOT_USERNAME;
 config.botPassword = process.env.TWITCH_BOT_PASSWORD;
 
+// Twitch API details
+config.twitchBotClientId = process.env.TWITCH_BOT_CLIENT_ID;
+config.twitchBotClientSecret = process.env.TWITCH_BOT_CLIENT_SECRET;
+
 // Bot debug (set to 'false' in production)
 config.useDebug = config.nodeEnv === "production" ? false : true;
 
